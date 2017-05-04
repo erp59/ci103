9987d06b4e20654645a9f13353083d59b8a677ee
ASUS-Laptop\rwp11 <rwp34@drexel.edu>
2017-05-04 13:50:07 -0400

    UI Changes

--- studentView/login.php
+++ studentView/login.php
@@ -1,13 +1,3 @@
-
-<?php
-
-
-session_start();
-
-
-?>
-
-
 <html>
 	<head><title>Quik Swipe Login</title>
 	
@@ -15,9 +5,10 @@ session_start();
 	<style>
 	
 	input[type=text], select {
-    width: 50%;
+    width: 150%;
     padding: 12px 20px;
-    margin: 8px 0;
+    margin: 0 auto;
+	text-align: center;
     display: inline-block;
     border: 1px solid #ccc;
     border-radius: 4px;
@@ -25,9 +16,10 @@ session_start();
 	}
 	
 	input[type=password], select {
-    width: 50%;
+    width: 150%;
     padding: 12px 20px;
     margin: 8px 0;
+	text-align: center;
     display: inline-block;
     border: 1px solid #ccc;
     border-radius: 4px;
@@ -35,24 +27,30 @@ session_start();
 	}
 
 	input[type=submit] {
-    width: 50%;
+    width: 200%;
     background-color: #4CAF50;
     color: white;
     padding: 14px 20px;
-    margin: 8px 0;
     border: none;
     border-radius: 4px;
     cursor: pointer;
 	}
 
-input[type=submit]:hover {
+	input[type=submit]:hover {
     background-color: #45a049;
 	}
 
-div {
+	div {
+	text-align: center;
     border-radius: 5px;
     background-color: f2f2f2;
     padding: 20px;
+	width: 30%;
+	margin: 0 auto;
+	}
+	
+	body {
+		background-image: url("dragon.jpg");
 	}
 	
 	</style>
@@ -69,18 +67,17 @@ div {
   	<div>
   	<form action="signInChecker.php" Method= "post">
   	
-  	<label for="error"><?php echo $error;?></label> <br />
-  	
   	<label for="username">Username</label> <br />
-    <input type="text" id="username" name="username" placeholder="abc123">
+    <input style="text-align: center;" type="text" id="username" name="username" placeholder="Username">
 	<br />
 	
     <label for="password">Password</label><br />
-    <input type="password" id="password" name="password">
+    <input type="password" id="password" name="password" placeholder="Password">
     <br />
     <input type="submit" value="Sign In">
   	
   	</form>
+	</div>
   	
 	</body>
 	
