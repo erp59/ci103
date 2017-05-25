6130e6d2541e3342361564b76f62d6f1b7cf91cc
Elio Pettigrosso <erp59@drexel.edu>
2017-05-25 13:10:42 -0400

    Changes to history table

--- sql/history.sql
+++ sql/history.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: May 23, 2017 at 10:07 PM
+-- Generation Time: May 25, 2017 at 07:09 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -21,21 +21,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `history` (
-  `id` int(8) NOT NULL,
-  `ruserName` varchar(10) NOT NULL,
-  `gFirstName` varchar(30) NOT NULL,
-  `gLastName` varchar(30) NOT NULL,
+  `ID` int(6) NOT NULL,
+  `rID` int(8) NOT NULL,
+  `rusername` varchar(10) NOT NULL,
+  `gusername` varchar(30) NOT NULL,
+  `gfn` varchar(30) NOT NULL,
+  `gln` varchar(30) NOT NULL,
+  `gtnumber` int(12) NOT NULL,
   `hall` varchar(30) NOT NULL,
-  `dateIn` varchar(30) NOT NULL,
-  `timeIn` varchar(30) NOT NULL,
-  `dateOut` varchar(30) NOT NULL,
-  `timeOut` varchar(30) NOT NULL,
-  `guserName` varchar(10) DEFAULT NULL
+  `din` varchar(30) NOT NULL,
+  `tin` varchar(30) NOT NULL,
+  `dout` varchar(30) NOT NULL,
+  `tout` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `history`
 --
 
-INSERT INTO `history` (`id`, `ruserName`, `gFirstName`, `gLastName`, `hall`, `dateIn`, `timeIn`, `dateOut`, `timeOut`, `guserName`) VALUES
-(12569439, 'erp59', 'Franco', 'Pettigrosso', 'race', '123wf', '12312', '12312', '2332', NULL);
+INSERT INTO `history` (`ID`, `rID`, `rusername`, `gusername`, `gfn`, `gln`, `gtnumber`, `hall`, `din`, `tin`, `dout`, `tout`) VALUES
+(0, 12569439, 'erp59', '', 'Franco', 'Pettigrosso', 0, 'race', '123wf', '12312', '12312', '2332'),
+(755929, 12345678, 'jfc22', 'jlg26', 'Mike', 'Brown', 9640853, 'Race', 'May 23', '10:00AM', 'May 24', '11:00AM');
